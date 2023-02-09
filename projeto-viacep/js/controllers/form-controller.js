fa98c353a9d8b37cd754bec76a8e7bb42d616671
Rafael Santos <rafa.05125@gmail.com>
2023-02-09 11:15:07 -0300

    ViaCEP API first test

--- projeto-viacep/js/controllers/form-controller.js
+++ projeto-viacep/js/controllers/form-controller.js
@@ -1,4 +1,5 @@
 import Address from "../models/address.js";
+import * as requestService from '../services/request-service.js';
 
 function State() {
 
@@ -32,6 +33,15 @@ export function init() {
 
     state.inputNumber.addEventListener('change', handleInputNumberChange);
     state.btnClear.addEventListener('click', handleBtnClearClick);
+    state.btnSave.addEventListener('click', handleBtnSaveClick);
+
+    requestService.getJson('https://viacep.com.br/ws/01001000/json/');
+}
+
+async function handleBtnSaveClick(event) {
+    event.preventDefault();
+    const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/');
+    console.log(result);
 }
 
 function handleInputNumberChange(event) {
