3bcefd227e62358a2be8f08aaee97f8a780fe75d
Rafael Santos <rafa.05125@gmail.com>
2023-02-09 15:03:19 -0300

    HandleInputNumberKeyup

--- projeto-viacep/js/controllers/form-controller.js
+++ projeto-viacep/js/controllers/form-controller.js
@@ -33,10 +33,15 @@ export function init() {
     state.errorNumber = document.querySelector('[data-error="number"]');
 
     state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.inputNumber.addEventListener('keyup', handleInputNumberKeyup);
     state.btnClear.addEventListener('click', handleBtnClearClick);
     state.btnSave.addEventListener('click', handleBtnSaveClick);
     state.inputCep.addEventListener('change', handleInputCepChange);
 
+}
+
+function handleInputNumberKeyup(event) {
+    state.address.number = event.target.value;
 }
 
 async function handleInputCepChange(event) {
@@ -56,12 +61,11 @@ async function handleInputCepChange(event) {
         state.inputCity.value = "";
         setFormError("cep", "Informe um CEP válido")
     }
-
 }
 
 async function handleBtnSaveClick(event) {
     event.preventDefault();
-    console.log(event.target);
+    console.log(state.address);
 }
 
 function handleInputNumberChange(event) {
