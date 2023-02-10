76a7a658e44b2f5c1052a9367c24283a3f6c0c89
Rafael Santos <rafa.05125@gmail.com>
2023-02-10 01:50:33 -0300

    ModalController, PageController

--- projeto-viacep/js/dsviacep.js
+++ projeto-viacep/js/dsviacep.js
@@ -1,5 +1,9 @@
 import * as formController from './controllers/form-controller.js';
 import * as listContoller from './controllers/list-controller.js';
+import * as modalController from './controllers/modal-controller.js';
+import * as pageController from './controllers/page-controller.js';
 
 formController.init();
 listContoller.init();
+modalController.init();
+pageController.init();
